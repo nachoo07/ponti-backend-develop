ae9c9ecfea6bb804c3c06f54b6b9b75feecbfa7e
Ignacio Skibski <nanoskibski@gmail.com>
2026-01-08 15:50:41 -0300

    Fix Cloud Run: change HTTP_SERVER_HOST default to 0.0.0.0

--- projects/ponti-api/cmd/config/http_server.go
+++ projects/ponti-api/cmd/config/http_server.go
@@ -7,7 +7,7 @@ import (
 
 type HTTPServer struct {
 	Name string `envconfig:"HTTP_SERVER_NAME" default:"http-server" validate:"required"`
-	Host string `envconfig:"HTTP_SERVER_HOST" default:"localhost" validate:"required"`
+	Host string `envconfig:"HTTP_SERVER_HOST" default:"0.0.0.0" validate:"required"`
 	Port int    `envconfig:"HTTP_SERVER_PORT" default:"8080" validate:"gte=0"`
 }
 
